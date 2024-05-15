540202b214bac5f9727bfaf5618f14ec8a15020e
MinhQuan <nguyenminhquan20403@gmail.com>
2024-05-15 20:23:22 +0700

    merge thanh cong va chay dc bth cac tinh nang cua Hung va Truong

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,28 +12,6 @@ class product extends Model
     protected array $filltable = [
         'ID',
         'name',
-        'price',
-        'image',
-        'categoryID',
-
-    ];
-    public function categories(): \Illuminate\Database\Eloquent\Relations\BelongsTo
-    {
-        return $this-> belongsTo(Categories::class);
-    }
-}
-<?php
-
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Product extends Model
-{
-    use HasFactory;
-    protected $fillable = [
-        'name',
         'description',
         'status',
         'price',
@@ -41,6 +19,49 @@ class Product extends Model
         'featured',
         'categoryID',
         'branchID',
+
     ];
-   
+    public function categories(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this-> belongsTo(Categories::class);
+    }
 }
+
+// class product extends Model
+// {
+//     use HasFactory;
+//     protected array $filltable = [
+//         'ID',
+//         'name',
+//         'price',
+//         'image',
+//         'categoryID',
+
+//     ];
+//     public function categories(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+//     {
+//         return $this-> belongsTo(Categories::class);
+//     }
+// }
+// <?php
+
+// namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Model;
+
+// class Product extends Model
+// {
+//     use HasFactory;
+//     protected $fillable = [
+//         'name',
+//         'description',
+//         'status',
+//         'price',
+//         'image',
+//         'featured',
+//         'categoryID',
+//         'branchID',
+//     ];
+   
+// }
