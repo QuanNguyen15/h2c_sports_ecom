8757356f169fac732386998ecd64aaaddb893e71
MinhQuan <nguyenminhquan20403@gmail.com>
2024-05-18 11:20:32 +0700

    merge

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -5,44 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Product;
-
+use App\Models\categories;
 
 class Categories extends Model
 {
-
     use HasFactory;
     protected $table = 'categories';
+    protected $fillable = [
+        'ID',
+        'category',
+        'slug',
 
-    protected array $filltable = [
-              'ID',
-              'category',
-              'slug'
 
     ];
-    public function product(): \Illuminate\Database\Eloquent\Relations\HasMany
-    {
-          return $this-> hasMany(Product::class,'categoryID','ID');
+
+
+    public function product(){
+        return $this->hasMany(Product::class, 'categoryID', 'ID');
     }
-
-
-
-    public $timestamps = false;
-
 }
-// <?php
-
-// namespace App\Models;
-
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
-
-// class categories extends Model
-// {
-//     use HasFactory;
-//     protected $fillable = [
-//         'category',
-//         'slug'
-//     ];
-//     public $timestamps = false;
-
-// }
