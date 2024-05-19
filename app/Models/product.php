55d3507e273e5d1a25dce415bc33f9314a5f84ec
MinhQuan <nguyenminhquan20403@gmail.com>
2024-05-19 21:07:11 +0700

    sua product file

--- app/Models/product.php
+++ app/Models/product.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Categories;
 
-class Product extends Model
+class product extends Model
 {
     use HasFactory;
+    protected $table = 'product';
+
+    protected $primaryKey = 'ID';
+
     protected $fillable = [
         'name',
         'description',
@@ -15,8 +20,37 @@ class Product extends Model
         'price',
         'image',
         'featured',
+        'create_at',
+        'update_at',
+        'delete_at',
         'categoryID',
         'branchID',
+
     ];
-   
+
+    public function getById($id)
+    {
+        return Product::findOrFail($id); // Sử dụng Eloquent để lấy sản phẩm theo ID
+    }
+
+    public function getAll()
+    {
+        return Product::all();
+    }
+
+    public function getByCategory($cateID)
+    {
+        return Product::where('categoryID', $cateID)->get();
+    }
+
+    public function category(){
+        return $this->belongsTo(Categories::class, 'categoryID', 'ID');
+    }
+
+    public function productImages()
+    {
+        return $this->hasMany(ProductImage::class, 'productID');
+    }
+
+
 }
