e46f345cdb989851fa5133798938ed4544c8772e
MinhQuan <nguyenminhquan20403@gmail.com>
2024-05-19 10:32:32 +0700

    merge code to deploy

--- app/Models/categories.php
+++ app/Models/categories.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Product;
-use App\Models\categories;
 
 class Categories extends Model
 {
