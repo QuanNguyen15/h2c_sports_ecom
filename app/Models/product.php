534f2a7555c123c26f4c7c305f919d5d5c6a5ecb
MinhQuan <nguyenminhquan20403@gmail.com>
2024-05-19 21:05:41 +0700

    change branch deploy product

--- app/Models/product.php
+++ app/Models/product.php
@@ -4,15 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Categories;
 
 class Product extends Model
 {
     use HasFactory;
-    protected $table = 'product';
-
-    protected $primaryKey = 'ID';
-
     protected $fillable = [
         'name',
         'description',
@@ -20,37 +15,8 @@ class Product extends Model
         'price',
         'image',
         'featured',
-        'create_at',
-        'update_at',
-        'delete_at',
         'categoryID',
         'branchID',
-
     ];
-
-    public function getById($id)
-    {
-        return Product::findOrFail($id); // Sử dụng Eloquent để lấy sản phẩm theo ID
-    }
-
-    public function getAll()
-    {
-        return Product::all();
-    }
-
-    public function getByCategory($cateID)
-    {
-        return Product::where('categoryID', $cateID)->get();
-    }
-
-    public function category(){
-        return $this->belongsTo(Categories::class, 'categoryID', 'ID');
-    }
-
-    public function productImages()
-    {
-        return $this->hasMany(ProductImage::class, 'productID');
-    }
-
-
+   
 }
